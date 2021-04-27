011124999e071a0ed11cab12c6ff33f2f9928dd8
AirplaneGoBrr <mottlcompound@gmail.com>
2021-04-27 10:02:13 -0700

    ??

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ var password = process.env.PASSWORD;
 var token = process.env.TOKEN;
 var prefix = process.env.DPREFIX;
 var chanel = process.env.DISCOCHANEL;
+var code = "";
 
 console.log("Done! Starting login");
 console.log(process.env.CHANNELS.split(","))
@@ -46,6 +47,51 @@ client.on('connected', onConnectedHandler);
 
 client.connect();
 
+//Discord stuff
+client2.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+  const user = message.mentions.users.first();
+  const member = message.guild.member(user);
+  const author = message.author.id;
+  const disconame = message.author.username;
+  const channel = message.channel.id;
+
+  if (message.content.startsWith(prefix+"say")){
+    split = message.content.split(" ");
+    console.log(username)
+    console.log(password)
+    console.log("Channel: "+split[1]+"\n"+split.slice(2).join(" "))
+    client.say(split[1], split.slice(2).join(" "));
+  }
+
+  if (message.content.startsWith(prefix+"link")){
+    split = message.content.split(" ");
+    var code = split[1]
+    message.reply("Please check DMs!")
+    try {
+      client2.users.cache.get(author)
+          .send("Hello <@"+author+"> your link code that you entered is: "+code+" \n Please enter the code in twitch chat with "+tprefix+"code <codehere>\n this doesnt work btw!")
+    } catch (error) {
+      console.log(error);
+      client2.reply("Error.");
+    }
+  }
+
+  if (!client2.commands.has(command)) return;
+
+	try {
+		client2.commands.get(command).execute(message, args, user, member);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+//Discord stuff ^
+
 //Twitch Stuff
 function onMessageHandler (target, context, msg, self) {
   console.log(target)
@@ -64,37 +110,16 @@ function onMessageHandler (target, context, msg, self) {
       console.log(error)
     }
   }
+
+  if (msg.startsWith(tprefix+"code")){
+    split = msg.split(" ")
+    if (split[1] == code){
+      client.say("Hello user "+disconame)
+    }
+  }
 }
 //Twitch Stuff ^
 
-//Discord stuff
-client2.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
-
-  const user = message.mentions.users.first();
-  const member = message.guild.member(user);
-
-  if (message.content.startsWith(prefix+"say")){
-    split = message.content.split(" ");
-    console.log(username)
-    console.log(password)
-    console.log("Channel: "+split[1]+"\n"+split.slice(2).join(" "))
-    client.say(split[1], split.slice(2).join(" "));
-  }
-
-  if (!client2.commands.has(command)) return;
-
-	try {
-		client2.commands.get(command).execute(message, args, user, member);
-	} catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
-});
-//Discord stuff ^
 
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port, target, context, msg, self) {
